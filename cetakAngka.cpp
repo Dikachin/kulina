eba45d273cf473117a52f815b8eb1537035734c3
Dikachin <andikarahimdarusalam@gmail.com>
2018-05-09 09:54:46 +0700

    Update cetakAngka.cpp

--- cetakAngka.cpp
+++ cetakAngka.cpp
@@ -1,19 +1,14 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-//input : 1.345.679
+
 int main(){
-	string str; cin>>str;
-	int l = str.length();
-	string zero="";
-	stack<string> ans;
-	for (int i=l-1; i>=0; i--){
-		if (str[i]=='.') continue;
-		ans.push(str[i]+zero);
-		zero+='0';
-	}
-	while(!ans.empty()){
-		cout<<ans.top()<<endl;
-		ans.pop();
+	int bilangan; cin>>bilangan;
+	int manyNol = log10(bilangan);
+	int nol = (int)(pow(10,manyNol));
+	while(nol>0){
+		int digit=bilangan/nol % 10;
+		cout<<digit*nol<<endl;
+		nol/=10;
 	}
 }
